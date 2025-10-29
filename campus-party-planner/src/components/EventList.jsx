5fdb7e9a3210c083cbd7a04288fc20f6c76cf3c1
Gaste <gastineaulouis@gmail.com>
2025-10-29 14:53:09 +0100

    Fix: Event list gets api data and uses EventCard

--- campus-party-planner/src/components/EventList.jsx
+++ campus-party-planner/src/components/EventList.jsx
@@ -1,13 +1,21 @@
-import { useContext } from "react";
+import { useEffect, useContext } from "react";
 import { EventContext } from "../context/EventContext";
-import EventCard from "./EventCard";
+import {EventCard} from "./EventCard";
 
 export function EventList() {
-  const { events } = useContext(EventContext);
+  const { events, selectedCity, setEvents } = useContext(EventContext);
+  
+  useEffect(() => {
+    fetchCities().then(setCities);
+  }, []);
+
+  const filteredEvents = events.filter((event) =>
+    selectedCity ? event.city === selectedCity : true
+  );
 
   return (
     <div className="event-list">
-      {events.map((event) => (
+      {filteredEvents.map((event) => (
         <EventCard key={event.id} event={event} />
       ))}
     </div>
