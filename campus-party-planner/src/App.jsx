41a237d6bd23bf64dc7ee88f8242071ee6907182
CodeCilla <priscilla.renault72460@gmail.com>
2025-10-29 14:05:10 +0100

    fix - change path header

--- campus-party-planner/src/App.jsx
+++ campus-party-planner/src/App.jsx
@@ -1,4 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import {EventProvider} from './context/EventContext';
 import HomePage from './pages/HomePage';
 import StatsPage from './pages/StatsPage';
 import './App.css';
@@ -6,15 +7,17 @@ import './App.css';
 
 function App() {
   return(
-<Router>
-  <main>
-    <Routes>
-      <Route path='/' element={<HomePage />} />
-      <Route path='/stats' element={<StatsPage />} />
-    </Routes>
-  </main>
-</Router>
-);
+    <EventProvider>
+      <Router>
+        <main>
+          <Routes>
+            <Route path='/' element={<HomePage />} />
+            <Route path='/stats' element={<StatsPage />} />
+          </Routes>
+        </main>
+      </Router>
+    </EventProvider>
+  );
 }
 export default App;
 
