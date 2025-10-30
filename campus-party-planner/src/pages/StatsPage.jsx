0c3887eb1d8e60d50ae8c9dfc13431314888ab4c
Gaste <gastineaulouis@gmail.com>
2025-10-30 10:23:47 +0100

    added category selector to statspage

--- campus-party-planner/src/pages/StatsPage.jsx
+++ campus-party-planner/src/pages/StatsPage.jsx
@@ -1,15 +1,25 @@
 import StatsChart from '../components/StatsChart';
 import Header from '../components/Header';
 import CitySelector from '../components/CitySelector';
+import CategorySelector from '../components/CategorySelector';
 import '../styles/pages/StatsPage.css';
 
 function StatsPage() {
   return (
     <div className="stats-page">
       <Header />
-      <CitySelector />
+      <div className="selectors-wrapper">
+        <div className="selector">
+          <CitySelector />
+        </div>
+        <div className="selector">
+          <CategorySelector />
+        </div>
+      </div>
+
       <h1>Statistiques</h1>
       <p>Ici vous pouvez trouver diverses statistiques sur les fêtes du campus!! 🥳​</p>
+
       <div className="stats-chart">
         <StatsChart />
       </div>
