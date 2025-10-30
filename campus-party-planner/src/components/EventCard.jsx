81a55c1501b434a93d8582c84f52d1951aaceb75
Gaste <gastineaulouis@gmail.com>
2025-10-30 15:57:46 +0100

    feat added darkmode to multiple sections

--- campus-party-planner/src/components/EventCard.jsx
+++ campus-party-planner/src/components/EventCard.jsx
@@ -1,44 +1,43 @@
-import { useContext } from 'react';
-import PropTypes from 'prop-types';
-import { FaHeart, FaRegHeart } from 'react-icons/fa'; // Utilise Font Awesome pour les icônes de cœur plein et vide
-import { EventContext } from '../context/EventContext';
+import { useContext } from "react";
+import PropTypes from "prop-types";
+import { FaHeart, FaRegHeart } from "react-icons/fa";
+import { EventContext } from "../context/EventContext";
 import "../styles/components/EventCard.css";
 
 const EventCard = ({ id, name, image, date, location, category, liked }) => {
-  const { toggleLike } = useContext(EventContext);
-
+  const { toggleLike, darkmode } = useContext(EventContext);
 
   const handleLikeClick = () => {
     toggleLike(id);
   };
 
   return (
-    <div className='event-card'>
-      <h2 className='event-card__name'>{name}</h2>
-      <img className='event-card__image' src={image} alt={name} />
-      <p className='event-card__location'>Lieu: {location}</p>
-      <p className='event-card__date'>Date : {date}</p>
-      <p className='event-card__category'>{category}</p>
-<button
-  className={`event-card__like-button ${liked ? 'liked' : ''}`}
-  onClick={handleLikeClick}
->
-  {liked ? <FaHeart /> : <FaRegHeart />}
-</button>
+    <div className={`event-card ${darkmode ? "event-card--dark" : ""}`}>
+      <h2 className="event-card__name">{name}</h2>
+      <img className="event-card__image" src={image} alt={name} />
+      <p className="event-card__location">Lieu : {location}</p>
+      <p className="event-card__date">Date : {date}</p>
+      <p className="event-card__category">{category}</p>
 
+      <button
+        className={`event-card__like-button ${liked ? "liked" : ""}`}
+        onClick={handleLikeClick}
+        aria-label={liked ? "Unlike" : "Like"}
+      >
+        {liked ? <FaHeart /> : <FaRegHeart />}
+      </button>
     </div>
   );
 };
 
 EventCard.propTypes = {
+  id: PropTypes.string.isRequired,
   name: PropTypes.string.isRequired,
   image: PropTypes.string.isRequired,
   date: PropTypes.string.isRequired,
   location: PropTypes.string.isRequired,
   category: PropTypes.string.isRequired,
-  onclick: PropTypes.func,
-  toggleLike: PropTypes.func.isRequired,
-  isLiked: PropTypes.bool.isRequired,
+  liked: PropTypes.bool.isRequired,
 };
 
 export default EventCard;
--- campus-party-planner/src/pages/HomePage.jsx
+++ campus-party-planner/src/pages/HomePage.jsx
@@ -3,20 +3,24 @@ import EventList from "../components/EventList.jsx";
 import CitySelector from "../components/CitySelector.jsx";
 import CategorySelector from "../components/CategorySelector.jsx";
 import "../styles/components/Selector.css";
+import "../styles/pages/HomePage.css";
+import { useContext } from "react";
+import { EventContext } from "../context/EventContext.jsx";
 function HomePage() {
+  const { darkmode } = useContext(EventContext);
   return (
-    <>
-      <Header />
-      <div className="selectors-wrapper">
-        <div className="selector">
-          <CitySelector />
+      <div className={`home-page ${darkmode ? "home-page--dark" : ""}`}>
+        <Header />
+        <div className="selectors-wrapper">
+          <div className="selector">
+            <CitySelector />
         </div>
         <div className="selector">
           <CategorySelector />
         </div>
       </div>
       <EventList />
-    </>
+    </div>
   );
 }
 export default HomePage;
